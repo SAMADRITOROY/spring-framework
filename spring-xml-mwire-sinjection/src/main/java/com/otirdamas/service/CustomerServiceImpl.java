e7471a4ffdfa86259079a92b83dfe9e901bef5bc
Samadrito Roy <samadrito.roy@gmal.com>
2019-04-08 11:02:03 +0530

    Initial Commit

---
+++ spring-xml-mwire-sinjection/src/main/java/com/otirdamas/service/CustomerServiceImpl.java
@@ -0,0 +1,21 @@
+package com.otirdamas.service;
+
+import java.util.List;
+
+import com.otirdamas.model.Customer;
+import com.otirdamas.repository.CustomerRepository;
+
+public class CustomerServiceImpl implements CustomerService {
+
+	private CustomerRepository customerRepository;
+
+	@Override
+	public List<Customer> findAll() {
+		return customerRepository.findAll();
+	}
+
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
+}
